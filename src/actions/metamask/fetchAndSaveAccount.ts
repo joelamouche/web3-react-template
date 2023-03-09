88d4e29aee0d3bb8c9bef92616350f745deb793d
antoine-dowgo <antoine@dowgo.com>
2023-03-09 11:38:40 +0100

    log accounts

--- src/actions/metamask/fetchAndSaveAccount.ts
+++ src/actions/metamask/fetchAndSaveAccount.ts
@@ -12,6 +12,7 @@ export async function fetchAndSaveAccount(dispatch: Dispatch<AppAction>) {
   ethereum.on("accountsChanged", handleAccountsChanged);
 
   function handleAccountsChanged(accounts: unknown) {
+    console.log('raw accounts',accounts)
     let accountList: EthAddress[] = [];
     if (accounts && (accounts as string[]).length) {
       accountList = accounts as EthAddress[];
