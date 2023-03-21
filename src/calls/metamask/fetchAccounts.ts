b3d8e034b438e01f430cc147e290e9f2a60ff2f8
antoine-dowgo <antoine@dowgo.com>
2023-03-21 11:45:38 +0100

    change to eth_requestAccounts

--- src/calls/metamask/fetchAccounts.ts
+++ src/calls/metamask/fetchAccounts.ts
@@ -4,7 +4,7 @@ export const fetchAccounts = async (): Promise<unknown> => {
   try {
     // @ts-ignore
     const ethereum = window.ethereum as MetaMaskInpageProvider;
-    const accounts = await ethereum.request({ method: "eth_accounts" });
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
 
     return accounts;
   } catch (error) {
