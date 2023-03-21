19110a74f60b6d607a6059f71b80f63214a41ac0
antoine-dowgo <antoine@dowgo.com>
2023-03-21 10:51:48 +0100

    log different addresses

--- src/actions/metamask/fetchAndSaveProvider.ts
+++ src/actions/metamask/fetchAndSaveProvider.ts
@@ -11,12 +11,16 @@ export async function fetchAndSaveProvider(dispatch: Dispatch<AppAction>) {
     // If the provider returned by detectEthereumProvider is not the same as
     // window.ethereum, something is overwriting it, perhaps another wallet.
   // @ts-ignore
+  console.log("comp",_provider,window.ethereum)
+    // @ts-ignore
     if (_provider !== window.ethereum) {
       console.error("Do you have multiple wallets installed?");
     }
     // Access the decentralized web!
     const provider = new ethers.providers.Web3Provider(_provider);
     dispatch({ type: "setProvider", value: provider });
+//@ts-ignore
+  console.log("accs",await window.ethereum.request({ method: "eth_accounts" }),await _provider.request({ method: "eth_accounts" }),await provider.listAccounts())
   } else {
     console.log("Please install MetaMask!");
     dispatch({ type: "setNeedInstallMetaMask", value: true });
